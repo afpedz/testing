aa4ed2bc34f5b222efc48fbd292dbe807f9c6baf
afpedz <75974066+afpedz@users.noreply.github.com>
2025-02-12 14:39:53 +0800

    Update index.php

--- view/index.php
+++ view/index.php
@@ -4,20 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home</title>
-        <link rel="stylesheet" href="../style.css">
-    </head>
-    <body>
-        <div class="card">
-            <h2>Welcome to the Website</h2>
-            <button onclick="window.location.href='../view/register.php'" >Register</button>
-            <button onclick="window.location.href='../view/login.php'"style="margin-top: 5px;">Login</button>
-        </div>
-    </body>
+    <div class="card">
+         <h2>Welcome to the Website</h2>
+         <button onclick="window.location.href='../view/register.php'" >Register</button>
+         <button onclick="window.location.href='../view/login.php'"style="margin-top: 5px;">Login</button>
+    </div>
 </body>
 </html>
